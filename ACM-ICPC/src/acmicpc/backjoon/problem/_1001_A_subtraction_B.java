36df0223bd67c4f93b53f5e9ec1002111fc61837
jeongseok <jeongseok912@gmail.com>
2018-08-01 01:06:09 +0900

    Upload ACM-ICPC problems

---
+++ ACM-ICPC/src/acmicpc/backjoon/problem/_1001_A_subtraction_B.java
@@ -0,0 +1,28 @@
+package acmicpc.backjoon.problem;
+
+import java.util.Scanner;
+
+/*
+문제
+두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A-B를 출력한다.
+
+예제 입력 1 
+3 2
+
+예제 출력 1 
+1
+*/
+public class _1001_A_subtraction_B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a-b);
+    }
+}
